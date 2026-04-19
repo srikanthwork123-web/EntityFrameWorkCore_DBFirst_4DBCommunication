a8b638c7df451d1b3f12160bd85c4425004070cc
Dell <Dell@DESKTOP-13B42NJ>
2026-04-19 09:18:27 +0530

    UserStory-123480-Created Departmentcontroller and DepartmentService and Departrepository by using DbFirstApproach

---
+++ EntityFrameWorkCore_DBFirst_4DBCommunication/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using EntityFrameWorkCore_DBFirst_4DBCommunication.NorthWind_DbModels;
+namespace EntityFrameWorkCore_DBFirst_4DBCommunication.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        Task<List<Department>> GetDepartments();
+        Task<Department> GetDepartmentById(int deptid);
+        Task<int> AddDepartments(Department deptdetail);
+        Task<bool> DeleteDepartmentById(int deptid);
+        Task<bool> UpdateDepartment(Department deptdetail);
+    }
+}
